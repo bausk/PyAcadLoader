8904376a91ca00830c9c0ff4ab57c1bcf900750d
Alex Bausk <bauskas@gmail.com>
2013-03-07 00:39:04 +0200

    Initial commit

---
+++ PyAcadLoader/Class1.cs
@@ -0,0 +1,108 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.EditorInput;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+
+namespace PythonLoader
+{
+    public class CommandsAndFunctions
+    {
+        [CommandMethod("-PYLOAD")]
+        public static void PythonLoadCmdLine()
+        {
+            PythonLoad(true);
+        }
+
+        [CommandMethod("PYLOAD")]
+        public static void PythonLoadUI()
+        {
+            PythonLoad(false);
+        }
+
+        public static void PythonLoad(bool useCmdLine)
+        {
+            Document doc =
+              Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+
+            short fd =
+              (short)Application.GetSystemVariable("FILEDIA");
+
+            // As the user to select a .py file
+
+            PromptOpenFileOptions pfo =
+                new PromptOpenFileOptions(
+                  "Select Python script to load"
+                );
+            pfo.Filter = "Python script (*.py)|*.py";
+            pfo.PreferCommandLine =
+              (useCmdLine || fd == 0);
+            PromptFileNameResult pr =
+              ed.GetFileNameForOpen(pfo);
+
+            // And then try to load and execute it
+
+            if (pr.Status == PromptStatus.OK)
+                ExecutePythonScript(pr.StringResult);
+        }
+
+        [LispFunction("PYLOAD")]
+        public ResultBuffer PythonLoadLISP(ResultBuffer rb)
+        {
+            const int RTSTR = 5005;
+
+            Document doc =
+              Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+
+            if (rb == null)
+            {
+                ed.WriteMessage("\nError: too few arguments\n");
+            }
+            else
+            {
+                // We're only really interested in the first argument
+
+                Array args = rb.AsArray();
+                TypedValue tv = (TypedValue)args.GetValue(0);
+
+                // Which should be the filename of our script
+
+                if (tv != null && tv.TypeCode == RTSTR)
+                {
+                    // If we manage to execute it, let's return the
+                    // filename as the result of the function
+                    // (just as (arxload) does)
+
+                    bool success =
+                      ExecutePythonScript(Convert.ToString(tv.Value));
+                    return
+                      (success ?
+                        new ResultBuffer(
+                          new TypedValue(RTSTR, tv.Value)
+                        )
+                        : null);
+                }
+            }
+            return null;
+        }
+
+        private static bool ExecutePythonScript(string file)
+        {
+            // If the file exists, let's load and execute it
+            // (we could/should probably add some more robust
+            // exception handling here)
+
+            bool ret = System.IO.File.Exists(file);
+            if (ret)
+            {
+                ScriptEngine engine = Python.CreateEngine();
+                engine.ExecuteFile(file);
+            }
+            return ret;
+        }
+    }
+}
